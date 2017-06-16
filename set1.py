31a278032224c5d48da5077ae0ee4a13e01b6c02
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-06-16 13:16:38 +0530

    Solve set 1 challenge 2

--- set1.py
+++ set1.py
@@ -3,8 +3,18 @@ import base64
 def hextobase64(inp):
 	try:
 		assert type(inp) is str
-	except:
+	except AssertionError:
 		print('The argument must be of type string (str)!')
 		raise
 
-	return base64.b64encode(bytes.fromhex(inp))
+	return base64.b64encode(bytes.fromhex(inp)).hex()
+
+def fixedxor(inp1, inp2):
+	try:
+		assert type(inp1) is str
+		assert type(inp2) is str
+	except AssertionError:
+		print('The argument must be of type string (str)!')
+		raise
+
+	return bytes(a ^ b for a, b in zip(bytes.fromhex(inp1), bytes.fromhex(inp2))).hex()
